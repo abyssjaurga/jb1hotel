7a81f972bbf3895aa09a4e82b92e0d9eec092bb7
riabsakool@gmail.com <riabsakool2003@gmail.com>
2021-07-08 10:15:55 +0700

    latest all project updated

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CacheImages from "./AssetLoader";
+import { AppLoading } from "expo";
+import Login from "./src/screens/Login";
+
+
+const _loadAssetsAsync = async () => {
+  const imageAssets = CacheImages([require("./src/assets/bg.png")]);
+  await Promise.all([...imageAssets]);
+};
+
+const App = () => {
+  const [isready, setIsready] = useState(false);
+
+  if (!isready) {
+    return (
+      <AppLoading
+        startAsync={_loadAssetsAsync}
+        onFinish={() => setIsready(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return <Login />;
+};
+
+export default App;
---
+++ src/screens/SignupForm.js
@@ -0,0 +1,22 @@
+import react from "react";
+import {Text, Button, View} from "react-native";
+
+class SignupForm {
+
+
+
+  render () {
+    
+    <View> 
+       <Text style = {{flex:1, flexDirection: 'row', justifyContent: 'center', FontSize: 30}}>
+         
+         Test Page Number 2 from button clicked
+
+       </Text>
+      
+      
+    </View>
+
+  }
+      
+}
